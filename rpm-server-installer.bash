db5d8ab81c6d93f7d10d3377cba730faaa156bec
Yolman Torrez <yolmat24@outlook.com>
2017-06-15 18:23:59 -0700

    Update rpm-server-installer.bash

--- rpm-server-installer.bash
+++ rpm-server-installer.bash
@@ -15,4 +15,6 @@ sudo sed -i 's,enabled=0,enabled=1,g' /etc/yum.repos.d/CentOS-Sources.repo
 
 #try to download rpm packages
 #for example yumdowloader --source tree
-
+#to generate an RPM package you need to create a source of the scripts you will package and create the .spec file
+#then create an tar.gz file fromm the directory in the /SOURCES with tar czvf file.tar.gz file
+#create the RPM rpmbuild -bb SPECS/file.spec or rpmbuild -v -bb --clean SPECS/file.spec
